46716952d799d48ad293c9372f7a52cf2c3e29b0
Ruyanur <148321146+Ruyanur@users.noreply.github.com>
2024-07-01 00:16:49 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace cilentform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.btnRequestSession = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtSessionTime = new System.Windows.Forms.TextBox();
+            this.lstMessageLog = new System.Windows.Forms.ListBox();
+            this.lblPermission = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSendMessage
+            // 
+            this.btnSendMessage.Location = new System.Drawing.Point(536, 53);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(75, 23);
+            this.btnSendMessage.TabIndex = 0;
+            this.btnSendMessage.Text = "button1";
+            this.btnSendMessage.UseVisualStyleBackColor = true;
+            // 
+            // btnRequestSession
+            // 
+            this.btnRequestSession.Location = new System.Drawing.Point(536, 83);
+            this.btnRequestSession.Name = "btnRequestSession";
+            this.btnRequestSession.Size = new System.Drawing.Size(75, 23);
+            this.btnRequestSession.TabIndex = 1;
+            this.btnRequestSession.Text = "button2";
+            this.btnRequestSession.UseVisualStyleBackColor = true;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(316, 37);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(100, 20);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // txtSessionTime
+            // 
+            this.txtSessionTime.Location = new System.Drawing.Point(316, 85);
+            this.txtSessionTime.Name = "txtSessionTime";
+            this.txtSessionTime.Size = new System.Drawing.Size(100, 20);
+            this.txtSessionTime.TabIndex = 3;
+            // 
+            // lstMessageLog
+            // 
+            this.lstMessageLog.FormattingEnabled = true;
+            this.lstMessageLog.Location = new System.Drawing.Point(88, 53);
+            this.lstMessageLog.Name = "lstMessageLog";
+            this.lstMessageLog.Size = new System.Drawing.Size(120, 95);
+            this.lstMessageLog.TabIndex = 4;
+            // 
+            // lblPermission
+            // 
+            this.lblPermission.AutoSize = true;
+            this.lblPermission.Location = new System.Drawing.Point(131, 207);
+            this.lblPermission.Name = "lblPermission";
+            this.lblPermission.Size = new System.Drawing.Size(35, 13);
+            this.lblPermission.TabIndex = 5;
+            this.lblPermission.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPermission);
+            this.Controls.Add(this.lstMessageLog);
+            this.Controls.Add(this.txtSessionTime);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnRequestSession);
+            this.Controls.Add(this.btnSendMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.Button btnRequestSession;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.TextBox txtSessionTime;
+        private System.Windows.Forms.ListBox lstMessageLog;
+        private System.Windows.Forms.Label lblPermission;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace cilentform
+{
+    public partial class Form1 : Form
+    {
+        private ClientDevice client;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DisableControls();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Form yüklenirken gereken işlemler
+            client = new ClientDevice("server_ip_address", 1234, "client_id");
+            client.Start();
+            SetClient(client);
+        }
+
+        public void SetClient(ClientDevice client)
+        {
+            this.client = client;
+        }
+
+        public void EnableControls()
+        {
+            txtMessage.Enabled = true;
+            btnSendMessage.Enabled = true;
+            btnRequestSession.Enabled = false;
+            txtSessionTime.Enabled = false;
+        }
+
+        public void DisableControls()
+        {
+            txtMessage.Enabled = false;
+            btnSendMessage.Enabled = false;
+            btnRequestSession.Enabled = true;
+            txtSessionTime.Enabled = true;
+        }
+
+        public void UpdatePermission(Permission permission)
+        {
+            lblPermission.Text = $"Permission: {permission}";
+        }
+
+        public void AddMessageToLog(string message)
+        {
+            lstMessageLog.Items.Add(message);
+        }
+
+        private void btnRequestSession_Click(object sender, EventArgs e)
+        {
+            int sessionTime;
+            if (int.TryParse(txtSessionTime.Text, out sessionTime))
+            {
+                client.RequestSession(sessionTime);
+            }
+            else
+            {
+                MessageBox.Show("Lütfen geçerli bir oturum süresi girin.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtMessage.Text))
+            {
+                string message = txtMessage.Text;
+                client.SendMessage(message);
+                txtMessage.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir mesaj girin.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    public class ClientDevice
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private string clientId;
+
+        public ClientDevice(string serverIpAddress, int serverPort, string clientId)
+        {
+            client = new TcpClient(serverIpAddress, serverPort);
+            stream = client.GetStream();
+            this.clientId = clientId;
+        }
+
+        public void Start()
+        {
+            Thread receiveThread = new Thread(ReceiveMessages);
+            receiveThread.Start();
+        }
+
+        private void ReceiveMessages()
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                var message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                // Gelen mesajı işleme kodu buraya gelecek
+                // Örneğin:
+                // Form1.AddMessageToLog(message);
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+
+        public void RequestSession(int sessionTime)
+        {
+            string requestMessage = $"REQUEST|{clientId}|{sessionTime}";
+            byte[] buffer = Encoding.ASCII.GetBytes(requestMessage);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+
+    public enum Permission
+    {
+        None,
+        Basic,
+        Admin,
+        SuperAdmin
+    }
+}
